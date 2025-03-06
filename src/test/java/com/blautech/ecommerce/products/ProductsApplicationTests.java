1e4a4b6ca13a47a6bd589790f65f71de2b1a7042
ahincho <ahincho@unsa.edu.pe>
2025-03-05 21:45:12 -0500

    Start Spring Boot Application

---
+++ src/test/java/com/blautech/ecommerce/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blautech.ecommerce.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
