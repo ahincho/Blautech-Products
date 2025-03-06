4942ad3367b811acd7bbb7fb0d591db211515a7c
ahincho <ahincho@unsa.edu.pe>
2025-03-05 23:43:57 -0500

    Product create request validation

--- src/main/java/com/blautech/ecommerce/products/infrastructure/adapters/in/rest/dtos/CreateOneProductRequest.java
+++ src/main/java/com/blautech/ecommerce/products/infrastructure/adapters/in/rest/dtos/CreateOneProductRequest.java
@@ -1,5 +1,9 @@
 package com.blautech.ecommerce.products.infrastructure.adapters.in.rest.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateOneProductRequest {
+    @NotBlank
+    @Size(min = 1, max = 256)
     private String name;
+    @Size(min = 1, max = 256)
     private String description;
+    @Positive
     private Double price;
+    @Positive
     private Integer quantity;
+    @Size(min = 1, max = 256)
     private String image;
 }
